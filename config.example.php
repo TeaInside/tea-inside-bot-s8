8b0f0bb44136a594e2d30f7ab546a7022e5ff7c8
Ammar Faizi <ammarfaizi2@gmail.com>
2020-07-14 11:55:57 +0700

    Update config example

--- config.example.php
+++ config.example.php
@@ -10,6 +10,9 @@ define("TELEGRAM_BOT_DB_PORT", "3306");
 define("TELEGRAM_BOT_DB_NAME", "teabot");
 define("TELEGRAM_BOT_SUDOERS", [243692601]);
 define("TELEGRAM_STORAGE_PATH", BASEPATH."/storage/telegram");
+define("TELEGRAM_WEBHOOK_KEY", "xxxxxxxxxxxxxxxxxxx");
+define("TELEGRAM_ERROR_REPORT_CHAT_ID", []);
+define("TELEGRAM_DAEMON_ERROR_LOG", STORAGE_PATH."/logs/telegram_daemon_error.log");
 
 define("SRABATSROBOT_API_KEY", "... token ...");
 
