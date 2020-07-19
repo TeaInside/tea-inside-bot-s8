4c611bfb423b067973d38a5a3fe4ae299e1540da
Ammar Faizi <ammarfaizi2@gmail.com>
2020-07-19 10:09:41 +0700

    Fix data insert in group logger

--- src/classes/TeaBot/Telegram/Loggers/GroupLogger.php
+++ src/classes/TeaBot/Telegram/Loggers/GroupLogger.php
@@ -28,6 +28,16 @@ class GroupLogger extends LoggerFoundation implements LoggerInterface
       ]
     );
 
+    $userId = self::userInsert(
+      [
+        "tg_user_id" => $this->data["user_id"],
+        "first_name" => $this->data["first_name"],
+        "last_name" => $this->data["last_name"],
+        "username" => $this->data["username"],
+        "is_bot" => $this->data["is_bot"] ? 1 : 0
+      ]
+    );
+
     /**
      * First check whether the tg_msg_id has
      * already been stored in database or not.
@@ -39,16 +49,31 @@ class GroupLogger extends LoggerFoundation implements LoggerInterface
     if ($u = $st->fetch(PDO::FETCH_ASSOC)) {
       
     } else {
+
+      /* Insert new message. */
       $pdo->prepare("INSERT INTO `tg_group_messages` (`group_id`, `user_id`, `tg_msg_id`, `reply_to_tg_msg_id`, `msg_type`, `has_edited_msg`, `is_forwarded_msg`, `tg_date`, `created_at`) VALUES (?, ?, ?, ?, 'text', ?, ?, ?, NOW())")
         ->execute(
           [
             $groupId,
-            $this->data["user_id"],
+            $userId,
             $this->data["msg_id"],
             $this->data["reply_to"]["message_id"] ?? null,
             $this->data["is_edited_msg"] ? 1 : 0,
             $this->data["is_forwarded_msg"] ? 1 : 0,
             date("Y-m-d H:i:s", $this->data["date"])
+          ]
+        );
+
+      $msgId = $pdo->lastInsertId();
+
+      $pdo->prepare("INSERT INTO `tg_group_message_data` (`msg_id`, `text`, `text_entities`, `file`, `is_edited`, `created_at`) VALUES (?, ?, ?, ?, ?, NOW());")
+        ->execute(
+          [
+            $msgId,
+            $this->data["text"],
+            json_encode($this->data["text_entities"], JSON_UNESCAPED_SLASHES),
+            null,
+            $this->data["is_edited_msg"] ? 1 : 0,
           ]
         );
     }
