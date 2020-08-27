5638ea43cad3f8c413a558768f6aa8bfe35a7214
Ammar Faizi <ammarfaizi2@gmail.com>
2020-08-27 14:51:06 +0700

    Clean up

--- src/classes/TeaBot/Telegram/Responses/Admin.php
+++ src/classes/TeaBot/Telegram/Responses/Admin.php
@@ -21,54 +21,6 @@ final class Admin extends ResponseFoundation
     -1001149709623  => true, /* Test Driven Development. */
   ];
 
-  /**
-   * @return bool
-   */
-  public function promote(): bool
-  {
-    if ($this->hasAbilityToPromoteOther()) {
-
-      if (!isset($this->data["reply_to"])) {
-        /* No replied message, ignoring... */
-        goto ret;
-      }
-
-      if (!isset($this->data["reply_to"]["from"]["id"])) {
-        /* No user_id to replied message, ignoring... */
-        goto ret;
-      }
-
-      $replyTo = $this->data["reply_to"];
-      $from    = $replyTo["from"];
-      $userId  = $from["id"];
-
-      $ret = json_decode(
-        Exe::promoteChatMember(
-          [
-            "chat_id"              => $this->data["chat_id"],
-            "user_id"              => $userId,
-            "can_change_info"      => true,
-            "can_delete_messages"  => true,
-            "can_invite_users"     => true,
-            "can_restrict_members" => true,
-            "can_pin_messages"     => true,
-            "can_promote_members"  => true,
-          ]
-        )->getBody()->__toString(),
-        true
-      );
-
-      $this->sendPromoteMessage(
-        $from["first_name"]
-        .(isset($from["last_name"]) ? " ".$from["last_name"] : ""),
-        $ret
-      );
-    }
-
-    ret:
-    return true;
-  }
-
 
   /**
    * @return bool
@@ -90,34 +42,85 @@ final class Admin extends ResponseFoundation
   /**
    * @return bool
    */
-  public function promoteMe(): bool
+  public function promote(): bool
   {
-    if (isset(self::PROMOTE_ME_ALLOWED_GROUPS[$this->data["chat_id"]])) {
-      $ret = json_decode(
-        Exe::promoteChatMember(
-          [
-            "chat_id"              => $this->data["chat_id"],
-            "user_id"              => $this->data["user_id"],
-            "can_change_info"      => true,
-            "can_delete_messages"  => true,
-            "can_invite_users"     => true,
-            "can_restrict_members" => true,
-            "can_pin_messages"     => true,
-            "can_promote_members"  => true,
-          ]
-        )->getBody()->__toString(),
-        true
-      );
-
-      $this->sendPromoteMessage(
-        $this->data["first_name"].(
-          isset($this->data["last_name"]) ? " ".$this->data["last_name"] : ""
-        ),
-        $ret
-      );
+    if (!$this->hasAbilityToPromoteOther()) {
+      /* Unauthorized user, ignoring... */
+      goto ret;
     }
 
+    if (!isset($this->data["reply_to"])) {
+      /* No replied message, ignoring... */
+      goto ret;
+    }
+
+    if (!isset($this->data["reply_to"]["from"]["id"])) {
+      /* No user_id to replied message, ignoring... */
+      goto ret;
+    }
+
+    $replyTo = $this->data["reply_to"];
+    $from    = $replyTo["from"];
+    $userId  = $from["id"];
+
+    /* Promote member here. */
+    $ret = self::promoteMember($userId, $this->data["chat_id"]);
+
+    $this->sendPromoteMessage(
+      $from["first_name"]
+      .(isset($from["last_name"]) ? " ".$from["last_name"] : ""),
+      $ret
+    );
+
+    ret:
     return true;
+  }
+
+
+  /**
+   * @return bool
+   */
+  public function promoteMe(): bool
+  {
+    if (!isset(self::PROMOTE_ME_ALLOWED_GROUPS[$this->data["chat_id"]])) {
+      /* Unauthorized group, ignoring... */
+      goto ret;
+    }
+
+    $ret = self::promoteMember($this->data["user_id"], $this->data["chat_id"]);
+    $this->sendPromoteMessage(
+      $this->data["first_name"]
+      .(isset($this->data["last_name"]) ? " ".$this->data["last_name"] : ""),
+      $ret
+    );
+
+    ret:
+    return true;
+  }
+
+  /**
+   * @param int $userId
+   * @param int $chatId
+   * @return mixed
+   */
+  private static function promoteMember(int $userId, int $chatId)
+  {
+    /* TODO: Custom privilege by parameters. */
+    return json_decode(
+      Exe::promoteChatMember(
+        [
+          "chat_id"              => $chatId,
+          "user_id"              => $userId,
+          "can_change_info"      => true,
+          "can_delete_messages"  => true,
+          "can_invite_users"     => true,
+          "can_restrict_members" => true,
+          "can_pin_messages"     => true,
+          "can_promote_members"  => true,
+        ]
+      )->getBody()->__toString(),
+      true
+    );
   }
 
 
@@ -129,28 +132,24 @@ final class Admin extends ResponseFoundation
   private function sendPromoteMessage(string $name, $ret): void
   {
     if (isset($ret["ok"], $ret["result"]) && $ret["ok"] && $ret["result"]) {
-
       $text =
         "<a href=\"tg://user?id={$this->data["user_id"]}\">"
         .htmlspecialchars($name, ENT_QUOTES, "UTF-8")
         ."</a> has been promoted to be an administrator!";
-
-      Exe::sendMessage(
-        [
-          "chat_id"             => $this->data["chat_id"],
-          "reply_to_message_id" => $this->data["msg_id"],
-          "text"                => $text,
-          "parse_mode"          => "HTML",
-        ]
-      );
-    } else {
-      Exe::sendMessage(
-        [
-          "chat_id"             => $this->data["chat_id"],
-          "reply_to_message_id" => $this->data["msg_id"],
-          "text"                => json_encode($ret, JSON_PRETTY_PRINT),
-        ]
-      );
+      $r = [
+        "chat_id"             => $this->data["chat_id"],
+        "reply_to_message_id" => $this->data["msg_id"],
+        "text"                => $text,
+        "parse_mode"          => "HTML",
+      ];
+    } else {      
+      $r = [
+        "chat_id"             => $this->data["chat_id"],
+        "reply_to_message_id" => $this->data["msg_id"],
+        "text"                => json_encode($ret, JSON_PRETTY_PRINT),
+      ];
     }
+
+    Exe::sendMessage($r);
   }
 }
