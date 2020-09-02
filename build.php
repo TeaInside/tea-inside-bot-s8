193cdfbcb9325feef710395c4e148281b178d8fa
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-02 16:43:01 +0700

    Clean up

--- build.php
+++ build.php
@@ -6,6 +6,9 @@ $swooleDir = __DIR__."/src/ext/swoole";
 $swooleLockFile = $swooleDir."/swoole_compiled.lock";
 $telegramConfigDir = __DIR__."/config/telegram";
 
+$tgExtDir =  __DIR__."/src/ext/tgbot";
+$tgExtLockFile = $tgExtDir."/tgbot.lock";
+
 function shw(...$argv)
 {
   $n = sh(...$argv);
@@ -15,53 +18,38 @@ function shw(...$argv)
   }
 }
 
-if (isset($argv[1]) && ($argv[1] === "release")) {
+/* Compile swoole */
+if (!file_exists($swooleLockFile)) {
+  shw($swooleDir, "phpize");
+  shw($swooleDir, "./configure --enable-openssl --enable-sockets --enable-http2 --enable-mysqlnd");
 
-  /* Compile swoole */
-  if (!file_exists($swooleLockFile)) {
-    shw($swooleDir, "phpize");
-    shw($swooleDir, "./configure --enable-openssl --enable-sockets --enable-http2 --enable-mysqlnd");
+  // Enable optimization.
+  file_put_contents($swooleDir."/Makefile",
+    str_replace(" -O0", " -O3", file_get_contents($swooleDir."/Makefile")));
 
-    // Enable optimization.
-    file_put_contents($swooleDir."/Makefile",
-      str_replace(" -O0", " -O3", file_get_contents($swooleDir."/Makefile")));
-
-    shw($swooleDir, "make");
-    shw($swooleDir, "sudo make install");
-    touch($swooleLockFile);
-  }
-
-  /*
-   * Build telegram config.
-   */
-  recursiveCallbackScanDir($telegramConfigDir, function (string $dir, string $file) {
-    if (preg_match("/^.+\.frag\.php$/S", $file)) {
-      shw(null, escapeshellarg(PHP_BINARY)." ".escapeshellarg($dir."/".$file));
-    }
-  });
-
-} else {
-
-  /* Compile swoole */
-  if (!file_exists($swooleLockFile)) {
-    shw($swooleDir, "phpize");
-    shw($swooleDir, "./configure --enable-openssl --enable-sockets --enable-http2 --enable-mysqlnd");
-
-    // Enable optimization.
-    file_put_contents($swooleDir."/Makefile",
-      str_replace(" -O0", " -O3", file_get_contents($swooleDir."/Makefile")));
-
-    shw($swooleDir, "make -j \$(nproc)");
-    shw($swooleDir, "sudo make install");
-    touch($swooleLockFile);
-  }
-
-  /**
-   * Build telegram config.
-   */
-  recursiveCallbackScanDir($telegramConfigDir, function (string $dir, string $file) {
-    if (preg_match("/^.+\.frag\.php$/S", $file)) {
-      shw(null, escapeshellarg(PHP_BINARY)." ".escapeshellarg($dir."/".$file));
-    }
-  });
+  shw($swooleDir, "make -j \$(nproc)");
+  shw($swooleDir, "sudo make install");
+  touch($swooleLockFile);
 }
+
+/* Compile custom extension. */
+if (!file_exists($tgExtLockFile)) {
+
+  shw($tgExtDir, "phpize");
+  shw($tgExtDir, "./configure");
+
+  file_put_contents($tgExtDir."/Makefile",
+    str_replace(" -O0", " -O3", file_get_contents($tgExtDir."/Makefile")));
+
+  shw($tgExtDir, "make -j \$(nproc)");
+  touch($tgExtLockFile);
+}
+
+/**
+ * Build telegram config.
+ */
+recursiveCallbackScanDir($telegramConfigDir, function (string $dir, string $file) {
+  if (preg_match("/^.+\.frag\.php$/S", $file)) {
+    shw(null, escapeshellarg(PHP_BINARY)." ".escapeshellarg($dir."/".$file));
+  }
+});
