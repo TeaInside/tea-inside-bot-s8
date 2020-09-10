48a76f973f846ec1ac1063ed22f599c30a95a79d
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-10 17:22:40 +0700

    Remove unused entries from config example

--- config.example.php
+++ config.example.php
@@ -2,19 +2,22 @@
 
 defined("BASEPATH") or define("BASEPATH", __DIR__);
 define("STORAGE_PATH", BASEPATH."/storage");
-define("TELEGRAM_BOT_TOKEN", ".... your bot token here ...");
-define("TELEGRAM_BOT_DB_HOST", "localhost");
+define("TELEGRAM_BOT_TOKEN", "xxxxxxxxxxxxxx");
+define("TELEGRAM_BOT_DB_HOST", "127.0.0.1");
 define("TELEGRAM_BOT_DB_USER", "root");
-define("TELEGRAM_BOT_DB_PASS", "asdqwe123zxcqwe");
+define("TELEGRAM_BOT_DB_PASS", "xxxxxx");
 define("TELEGRAM_BOT_DB_PORT", "3306");
-define("TELEGRAM_BOT_DB_NAME", "teabot");
-define("TELEGRAM_BOT_SUDOERS", [243692601]);
+define("TELEGRAM_BOT_DB_NAME", "teabot8");
+define("TELEGRAM_BOT_SUDOERS", [123123123]);
 define("TELEGRAM_STORAGE_PATH", BASEPATH."/storage/telegram");
-define("TELEGRAM_WEBHOOK_KEY", "xxxxxxxxxxxxxxxxxxx");
-define("TELEGRAM_ERROR_REPORT_CHAT_ID", []);
+define("TELEGRAM_WEBHOOK_KEY", "6e9b497c7ab7be32fd0be83502123490");
+define("TELEGRAM_ERROR_REPORT_CHAT_ID", [-100100000]);
 define("TELEGRAM_DAEMON_ERROR_LOG", STORAGE_PATH."/logs/telegram_daemon_error.log");
 define("TELEGRAM_DAEMON_LOG_FILE", STORAGE_PATH."/logs/daemon.log");
 define("TELEGRAM_DAEMON_PID_FILE", STORAGE_PATH."/pid/telegram_daemon.pid");
+
+define("TELEGRAM_DAEMON_MASTER_BA", "127.0.0.1:7000");
+define("TELEGRAM_MUTEXES_LOCK_DIR", TELEGRAM_STORAGE_PATH."/mutexes");
 
 define("TELEGRAM_DAEMON_LOGGER_WORKERS",
   [
@@ -33,8 +36,5 @@ define("TELEGRAM_DAEMON_RESPONDER_WORKERS",
     "127.0.0.1:7203",
   ]
 );
-
-define("CALCULUS_API_KEY", "xxxxxxxxx");
-define("SRABATSROBOT_API_KEY", "xxxxxxxxx");
 
 $ignoreAll = "*\n!.gitignore\n";
