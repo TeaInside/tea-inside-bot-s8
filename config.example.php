70f0047e37053024c91fad55da776f51ce3aa5d3
Ammar Faizi <ammarfaizi2@gmail.com>
2021-02-07 18:02:05 +0700

    Add config example
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ config.example.php
@@ -0,0 +1,26 @@
+<?php
+
+defined("BASEPATH") or define("BASEPATH", __DIR__);
+define("STORAGE_PATH", BASEPATH."/storage");
+
+
+/* Telegram config */
+define("TG_BOT_TOKEN", "bot token");
+define("TG_BOT_DB_HOST", "127.0.0.1");
+define("TG_BOT_DB_USER", "user");
+define("TG_BOT_DB_PASS", "password");
+define("TG_BOT_DB_PORT", 3306);
+define("TG_BOT_DB_NAME", "teabot8");
+define("TG_BOT_SUDOERS", [243692601]);
+define("TG_STORAGE_PATH", BASEPATH."/storage/telegram");
+define("TG_WEBHOOK_KEY", "123123123123123123");
+define("TG_ERROR_REPORT_CHAT_ID", []);
+define("TG_DAEMON_ERROR_LOG_FILE", STORAGE_PATH."/logs/telegram/error.log");
+define("TG_DAEMON_NOTICE_LOG_FILE", STORAGE_PATH."/logs/telegram/notice.log");
+define("TG_DAEMON_PID_FILE", STORAGE_PATH."/pids/tg_daemon.pid");
+
+
+
+
+
+define("CALCULUS_API_KEY", "8e7eaa2822cf3bf77a03d63d2fbdeb36df0a409f");
